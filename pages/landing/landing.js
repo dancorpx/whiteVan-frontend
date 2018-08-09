1186ac8d7c193880e64af1d3169af9ce0cc4d9f4
Callum Tipper <callum.tipper@gmail.com>
2018-08-09 15:44:36 +0800

    added the login for extra user info,should only appear the first time the user logs in

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -1,18 +1,23 @@
 // pages/landing/landing.js
-Page({
+const app = getApp();
+const globalData = app.globalData;
+const myRequest = require('../../lib/api/request');
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+Page({
+  data: { },
 
   goNext: function () {
-    wx.reLaunch({
-      url: '/pages/index/index',
-    })
-
+    console.log(9090, globalData)
+    if (globalData.userThiefId === null) {
+      console.log(101,"")
+      wx.reLaunch({
+        url: '/pages/login/login',
+      })
+    } else {
+      wx.reLaunch({
+        url: '/pages/index/index',
+      })
+    }
     
   },
 
